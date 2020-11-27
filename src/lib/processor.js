a4faf0dd8a73013514708def7bba8b40f828e786
cjus <Carlos.Justiniano@gmail.com>
2020-11-26 20:08:00 -0700

    first pass at task processor

--- src/lib/processor.js
+++ src/lib/processor.js
@@ -18,6 +18,13 @@ class Processor {
   init(config) {
     this.config = config;
     this.messageCheckDelay = 0;
+    this.lastExecutionSweep = 0;
+
+    setTimeout(async () => {
+      // check for executable tasks
+      await this.checkForExecutableTasks();
+    }, ONE_SECOND);
+
     this.checkForTasks();
   }
 
@@ -63,7 +70,67 @@ class Processor {
     }
     setTimeout(async () => {
       await this.checkForTasks();
+      this.lastExecutionSweep = ((new Date()).getTime() / 1000) | 0;
     }, this.messageCheckDelay);
+  }
+
+  /**
+   * @name checkForExecutableTasks
+   * @description check mongodb for tasks which are ready to execute
+   */
+  async checkForExecutableTasks() {
+    try {
+      let elapsedSeconds = (((new Date()).getTime() / 1000) | 0) - this.lastExecutionSweep;
+      let now = moment();
+      let topRange = moment();
+      topRange.subtract(elapsedSeconds, 'seconds');
+      let timeRangeQuery = `{$gt: ${topRange.toISOString()}}, {$lt: ${now.toISOString()}}`;
+
+      const taskColl = mdb.getCollection('tasks');
+      let tasks = await taskColl.find({
+        targetTime: timeRangeQuery,
+        suspended: false
+      }).toArray();
+
+      for await (let task of tasks) {
+        let mid = (task.rule.updateMid) ? uuid.v4() : task.message.mid;
+        let frm = (task.rule.updateFrm) ? `${hydra.getInstanceID()}@${hydra.getServiceName()}:/` : task.message.frm;
+        let msg = Object.assign({}, task.message, {
+          mid,
+          frm
+        });
+        if (task.rule.sendType === 'queue') {
+          await hydra.queueMessage(msg);
+        } else if (task.rule.sendType === 'send') {
+          if (task.rule.broadcast) {
+            await hydra.sendBroadcastMessage(msg);
+          } else {
+            await hydra.sendMessage(msg);
+          }
+        }
+
+        if (task.rule.frequency.oneTime) {
+          await taskColl.deleteOne({
+            taskID: task.taskID
+          });
+        } else {
+          let offset = moment();
+          offset.add(task.rule.frequency.offset[0], task.rule.frequency.offset[1]);
+          task.targetTime = offset.toISOString();
+          delete task.rule;
+          delete task.message;
+          await taskColl.updateOne({
+            taskID: task.taskID
+          },{
+            $set: task
+          },{
+            upsert: true
+          });
+        }
+      }
+    } catch (error) {
+      hydraExpress.log('fatal', e);
+    }
   }
 
   /**
@@ -75,6 +142,22 @@ class Processor {
   async queueSuccess(message) {
     let reply = hydra.createUMFMessage({
       to: message.frm,
+      frm: `${hydra.getInstanceID()}@${hydra.getServiceName()}:/`,
+      typ: message.typ,
+      bdy: message.bdy
+    }).toShort();
+    await hydra.queueMessage(reply);
+  }
+
+  /**
+   * @name queueSuccess
+   * @description queue a reply message for the sender with success
+   * @param {object} message - response message
+   * @return {undefined}
+   */
+  async queueMessage(message) {
+    let reply = hydra.createUMFMessage({
+      to: message.to,
       frm: `${hydra.getInstanceID()}@${hydra.getServiceName()}:/`,
       typ: message.typ,
       bdy: message.bdy
@@ -154,6 +237,7 @@ class Processor {
         let doc = {
           taskID: uuid.v4(),
           targetTime: offset.toISOString(),
+          suspended: false,
           rule: {
             frequency: parsedFrequency,
             sendType,
