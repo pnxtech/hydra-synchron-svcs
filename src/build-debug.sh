8226b43b050281c630afd16825677088df1e38c9
cjus <Carlos.Justiniano@gmail.com>
2022-04-13 12:41:05 -0600

    build update for Mac M1 support

--- src/build-debug.sh
+++ src/build-debug.sh
@@ -1,4 +1,5 @@
 # Syntax: ./build-debug.sh
 # Use --no-cache=true  when necessary
 VERSION_TAG=$(<VERSION)
-docker build -f Dockerfile.debug -t hydra-synchron-svcs:$VERSION_TAG .
+SERVICE_NAME=hydra-synchron-svcs
+docker build -f Dockerfile.debug --no-cache=true -t $SERVICE_NAME:$VERSION_TAG .
--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,6 @@
 # Syntax: ./build.sh
 # Use --no-cache=true  when necessary
 VERSION_TAG=$(<VERSION)
-docker build --no-cache=true -t hydra-synchron-svcs:$VERSION_TAG .
+SERVICE_NAME=hydra-synchron-svcs
+SERVICE_HOST=pnxtech
+docker buildx build --platform=linux/amd64,linux/arm64 --push --no-cache -t $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG .
